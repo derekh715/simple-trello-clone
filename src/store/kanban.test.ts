78a823afad318bc5b20116137f147e0a71ae0127
Derek Ho <derek715@protonmail.com>
2024-03-18 12:35:08 +0800

    implement and test kanban store

---
+++ src/store/kanban.test.ts
@@ -0,0 +1,170 @@
+import { createPinia, setActivePinia } from "pinia";
+import { beforeEach, describe, expect, it } from "vitest";
+import { Board, List, Task, useKanbanStore } from "./kanban";
+
+const expectedBoard: Omit<Board, "id" | "lists"> = {
+  description: "hello world",
+  name: "hello",
+};
+
+function createBoard(kanban: ReturnType<typeof useKanbanStore>) {
+  return kanban.addBoard(expectedBoard);
+}
+
+const expectedList: Omit<List, "id" | "tasks"> = {
+  description: "hello list",
+  name: "list",
+};
+
+function createList(kanban: ReturnType<typeof useKanbanStore>, board: Board) {
+  return kanban.addList(board.id, expectedList);
+}
+
+const expectedTask: Omit<Task, "id"> = {
+  name: "task",
+  status: "unfinished",
+  description: "hello task",
+};
+
+describe("boards", async () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it("should add a new board", () => {
+    const kanban = useKanbanStore();
+    createBoard(kanban);
+    expect(kanban.boards).toHaveLength(1);
+    expect(kanban.boards[0].description).toBe(expectedBoard.description);
+    expect(kanban.boards[0].name).toBe(expectedBoard.name);
+    expect(kanban.boards[0].lists).toEqual([]);
+    expect(kanban.boards[0].id).toBeDefined();
+  });
+
+  it("should remove board", () => {
+    const kanban = useKanbanStore();
+    const newBoard = createBoard(kanban);
+    kanban.removeBoard(newBoard.id);
+    expect(kanban.boards).toHaveLength(0);
+  });
+
+  it("should change board", () => {
+    const kanban = useKanbanStore();
+    let newBoard = createBoard(kanban);
+    newBoard = { ...newBoard, name: "changed" };
+    kanban.changeBoard(newBoard);
+    expect(kanban.boards[0].name).toBe("changed");
+  });
+});
+
+describe("list", async () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it("should add a new list", () => {
+    const kanban = useKanbanStore();
+    const newBoard = createBoard(kanban);
+    // createList returns a reference to the list inside newBoard
+    createList(kanban, newBoard);
+    expect(kanban.boards[0].lists).toHaveLength(1);
+    expect(kanban.boards[0].lists[0].description).toBe(
+      expectedList.description
+    );
+    expect(kanban.boards[0].lists[0].name).toBe(expectedList.name);
+    expect(kanban.boards[0].lists[0].tasks).toEqual([]);
+    expect(kanban.boards[0].lists[0].id).toBeDefined();
+  });
+
+  it("should remove list", () => {
+    const kanban = useKanbanStore();
+    const newBoard = createBoard(kanban);
+    const newList = createList(kanban, newBoard);
+    kanban.removeList(newBoard.id, newList!.id);
+    expect(kanban.boards[0].lists).toHaveLength(0);
+  });
+
+  it("should change list", () => {
+    const kanban = useKanbanStore();
+    const newBoard = createBoard(kanban);
+    const newList = createList(kanban, newBoard)!;
+    const changedList: List = { ...newList, name: "changed" };
+    kanban.changeList(newBoard.id, changedList);
+    expect(kanban.boards[0].lists[0].name).toBe("changed");
+  });
+});
+
+describe("tasks", async () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  function getList(kanban: ReturnType<typeof useKanbanStore>) {
+    const newBoard = createBoard(kanban);
+    createList(kanban, newBoard);
+    return { list: kanban.boards[0].lists[0], board: kanban.boards[0] };
+  }
+
+  it("should add a new task", () => {
+    const kanban = useKanbanStore();
+    const { list, board } = getList(kanban);
+    kanban.addTask(board.id, list.id, expectedTask);
+    expect(kanban.boards[0].lists[0].tasks).toHaveLength(1);
+    expect(kanban.boards[0].lists[0].tasks[0].description).toBe(
+      expectedTask.description
+    );
+    expect(kanban.boards[0].lists[0].tasks[0].name).toBe(expectedTask.name);
+  });
+
+  it("should remove task", () => {
+    const kanban = useKanbanStore();
+    const newBoard = createBoard(kanban);
+    const newList = createList(kanban, newBoard);
+    kanban.removeList(newBoard.id, newList!.id);
+    expect(kanban.boards[0].lists).toHaveLength(0);
+  });
+
+  it("should change task", () => {
+    const kanban = useKanbanStore();
+    const newBoard = createBoard(kanban);
+    const newList = createList(kanban, newBoard)!;
+    const changedList: List = { ...newList, name: "changed" };
+    kanban.changeList(newBoard.id, changedList);
+    expect(kanban.boards[0].lists[0].name).toBe("changed");
+  });
+
+  it("should rearrange tasks", () => {
+    const kanban = useKanbanStore();
+    const { list, board } = getList(kanban);
+    const task = kanban.addTask(board.id, list.id, {
+      name: "1",
+      status: "finished",
+      description: "",
+    })!;
+    kanban.addTask(board.id, list.id, {
+      name: "2",
+      status: "finished",
+      description: "",
+    });
+    expect(kanban.boards[0].lists[0].tasks).toHaveLength(2);
+    // move the first task to be behind the second task
+    kanban.rearrangeTask(board.id, list.id, task.id, 1);
+    // now the second task becomes the first task
+    expect(kanban.boards[0].lists[0].tasks[0].name).toBe("2");
+  });
+
+  it("should move task to a new list", () => {
+    const kanban = useKanbanStore();
+    const board = kanban.addBoard({ description: "", name: "" });
+    const l1 = kanban.addList(board.id, { description: "", name: "2" })!;
+    const l2 = kanban.addList(board.id, { description: "", name: "2" })!;
+    const task = kanban.addTask(board.id, l1.id, {
+      name: "1",
+      status: "finished",
+      description: "",
+    })!;
+    expect(kanban.boards[0].lists).toHaveLength(2);
+    kanban.rearrangeTaskToList(board.id, l1.id, task.id, l2.id, 0);
+    expect(kanban.boards[0].lists[1].tasks[0]).toEqual(task);
+  });
+});
--- src/store/kanban.ts
+++ src/store/kanban.ts
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import { defineStore } from "pinia";
 
 export interface KanbanState {
@@ -27,39 +28,176 @@ export interface Task {
   status: Status;
 }
 
-const exampleKanban: KanbanState = {
-  boards: [
-    {
-      name: "Example",
-      description: "",
-      id: "board-id",
-      lists: [
-        {
-          name: "List #1",
-          id: "list-id",
-          description: "",
-          tasks: [
-            {
-              id: "task-id",
-              name: "Task #1",
-              description: "Lorem ipsum sit amet",
-              status: "unfinished",
-            },
-            {
-              id: "task-id-2",
-              name: "Task #2",
-              description: "Lorem ipsum sit amet",
-              status: "finished",
-            },
-          ],
-        },
-      ],
-    },
-  ],
-};
+function findTaskIndex(
+  boards: Board[],
+  boardId: string,
+  listId: string,
+  taskId: string
+) {
+  const board = boards.find(({ id }) => boardId === id);
+  if (board) {
+    const list = board.lists.find(({ id }) => listId === id);
+    if (list) {
+      const taskIndex = list.tasks.findIndex(({ id }) => taskId === id);
+      if (taskIndex === -1) {
+        return undefined;
+      }
+      return {
+        taskIndex,
+        board,
+        list,
+      };
+    }
+  }
+
+  return undefined;
+}
+
+function findListIndex(boards: Board[], boardId: string, listId: string) {
+  const board = boards.find(({ id }) => boardId === id);
+  if (board) {
+    const listIndex = board.lists.findIndex(({ id }) => listId === id);
+    if (listIndex === -1) {
+      return undefined;
+    }
+    return { board, listIndex };
+  }
+  return undefined;
+}
 
 export const useKanbanStore = defineStore("kanban", {
   state: (): KanbanState => {
-    return exampleKanban;
+    return { boards: [] };
+  },
+
+  actions: {
+    addTask(boardId: string, listId: string, task: Omit<Task, "id">) {
+      const board = this.boards.find(({ id }) => boardId === id);
+      if (board) {
+        const list = board.lists.find(({ id }) => listId === id);
+        if (list) {
+          const newTask: Task = {
+            ...task,
+            id: nanoid(),
+          };
+          list.tasks.push(newTask);
+          return newTask;
+        }
+      }
+      return undefined;
+    },
+
+    removeTask(boardId: string, listId: string, taskId: string) {
+      const items = findTaskIndex(this.boards, boardId, listId, taskId);
+
+      if (items) {
+        const { taskIndex, list } = items;
+        list!.tasks.splice(taskIndex, 1);
+      }
+    },
+
+    changeTask(boardId: string, listId: string, taskId: string, newTask: Task) {
+      const items = findTaskIndex(this.boards, boardId, listId, taskId)!;
+      if (items) {
+        const { taskIndex, list } = items;
+        list.tasks[taskIndex] = newTask;
+      }
+    },
+
+    rearrangeTask(
+      boardId: string,
+      listId: string,
+      taskId: string,
+      newIndex: number
+    ) {
+      const items = findTaskIndex(this.boards, boardId, listId, taskId);
+
+      if (items) {
+        const { list, taskIndex } = items;
+        if (newIndex < 0 || newIndex >= list.tasks.length) {
+          return;
+        }
+        const [task] = list.tasks.splice(taskIndex, 1);
+        list.tasks.splice(newIndex, 0, task);
+      }
+    },
+
+    rearrangeTaskToList(
+      boardId: string,
+      listId: string,
+      taskId: string,
+      newListId: string,
+      newIndex: number
+    ) {
+      const taskItem = findTaskIndex(this.boards, boardId, listId, taskId)!;
+
+      const listItem = findListIndex(this.boards, boardId, newListId);
+
+      if (taskItem && listItem) {
+        const list = listItem.board.lists[listItem.listIndex];
+        // dont' use >= so that we can push an element at the end of a list
+        if (newIndex < 0 || newIndex > list.tasks.length) {
+          return;
+        }
+        const [task] = taskItem.list.tasks.splice(taskItem.taskIndex, 1);
+        list.tasks.splice(newIndex, 0, task);
+      }
+    },
+
+    addList(boardId: string, list: Omit<List, "id" | "tasks">) {
+      const board = this.boards.find(({ id }) => boardId === id);
+      if (board) {
+        const newList: List = {
+          ...list,
+          id: nanoid(),
+          tasks: [],
+        };
+        board.lists.push(newList);
+        return newList;
+      }
+      return undefined;
+    },
+
+    removeList(boardId: string, listId: string) {
+      const items = findListIndex(this.boards, boardId, listId);
+
+      if (items) {
+        const { listIndex, board } = items;
+        board.lists.splice(listIndex, 1);
+      }
+    },
+
+    changeList(boardId: string, newList: List) {
+      const items = findListIndex(this.boards, boardId, newList.id);
+      if (items) {
+        const { listIndex, board } = items;
+        board.lists.splice(listIndex, 1);
+        board.lists[listIndex] = newList;
+      }
+    },
+
+    addBoard(board: Omit<Board, "id" | "lists">) {
+      const newBoard: Board = {
+        ...board,
+        id: nanoid(),
+        lists: [],
+      };
+      this.boards.push(newBoard);
+      return newBoard;
+    },
+
+    removeBoard(boardId: string) {
+      const index = this.boards.findIndex(({ id }) => boardId === id);
+      if (index != -1) {
+        this.boards.splice(index, 1);
+      }
+    },
+
+    changeBoard(board: Board) {
+      const index = this.boards.findIndex(({ id }) => board.id === id);
+      if (index != -1) {
+        this.boards[index] = board;
+      }
+    },
   },
 });
